59a322d1c3b492a31aac9a72efde241861a0b0a4
Volodymyr” <krokhmalyuk777@gmail.com>
2024-01-08 19:59:47 +0200

    Task 1; Done with main logic

--- src/com/crypto/Main.java
+++ src/com/crypto/Main.java
@@ -1,7 +1,15 @@
 package com.crypto;
 
+import com.crypto.model.Mode;
+import com.crypto.service.DecryptService;
+
 public class Main {
     public static void main(String[] args) {
+        Mode someValueFromConsole = Mode.valueOf("Some value from console");
+        switch (someValueFromConsole) {
+            case DECRYPT:
+                new DecryptService().decrypt(null, 1);
+        }
 
     }
 }
