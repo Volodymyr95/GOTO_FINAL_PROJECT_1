58f8a11d9cc32733a2e2cec93fc54134ca664d05
IvanKa <ivankalnykit@gmail.com>
2024-01-26 14:04:56 +0100

    Added encrypt method

--- src/service/EncryptService.java
+++ src/service/EncryptService.java
@@ -2,7 +2,21 @@ package service;
 import java.io.File;
 public class EncryptService {
 
-    public void encrypt(File file, int key) {
+    public String encrypt(String text, int key) {
+        StringBuilder result = new StringBuilder();
+        for (char character : text.toCharArray()) {
+            if (Character.isLetter(character)) {
+                char base = Character.isUpperCase(character) ? 'A' : 'a';
+                char encryptedChar = (char) ((character - base + key) % 26 + base);
+                if (encryptedChar < base) {
+                    encryptedChar += 26;
+                }
+                result.append(encryptedChar);
+            } else {
+                result.append(character);
+            }
+        }
+        return result.toString();
+}
 
-    }
 }
