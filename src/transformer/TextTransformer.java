34921dd6109bb17a096e5628ff325849f9fa1776
IvanKa <ivankalnykit@gmail.com>
2024-01-26 11:49:32 +0100

    Added methods structure

--- src/transformer/TextTransformer.java
+++ src/transformer/TextTransformer.java
@@ -2,4 +2,11 @@ package transformer;
 
 public class TextTransformer {
 
+    public String moveLetterOnRightPosition(int pos, String text) {
+        return "";
+    }
+
+    public String moveLetterOnLeftPosition(int pos, String text) {
+        return "";
+    }
 }
