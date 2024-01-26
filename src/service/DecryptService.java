90163281f772c5bc32a6131d1355fadd7eaf94b6
IvanKa <ivankalnykit@gmail.com>
2024-01-26 14:05:20 +0100

    Added decrypt method

--- src/service/DecryptService.java
+++ src/service/DecryptService.java
@@ -1,4 +1,23 @@
 package service;
 
 public class DecryptService {
+
+    public String decrypt(String text, int key) {
+        StringBuilder result = new StringBuilder();
+
+        for (char character : text.toCharArray()) {
+            if (Character.isLetter(character)) {
+                char base = Character.isUpperCase(character) ? 'A' : 'a';
+                char shiftedChar = (char) ((character - base + key) % 26 + base);
+                if (shiftedChar < base) {
+                    shiftedChar += 26;  // Обработка отрицательного смещения
+                }
+                result.append(shiftedChar);
+            } else {
+                result.append(character);
+            }
+        }
+
+        return result.toString();
+    }
 }
