5a1d3ec13d0f0b720c1fed5e8e26300eac3a90bd
IvanKa <ivankalnykit@gmail.com>
2024-01-26 13:03:34 +0100

    Added methods readFileToString and writeStringToFile

--- src/IO/FileHandler.java
+++ src/IO/FileHandler.java
@@ -1,4 +1,28 @@
 package IO;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class FileHandler {
+
+    public String readFileToString(String filePath) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(filePath), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return content.toString();
+    }
+
+    public void writeStringToFile(String filePath, String content) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(filePath)))) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
