85477c5b739eca0978e343f100bfaddae99ed07b
IvanKa <ivankalnykit@gmail.com>
2024-01-27 15:34:40 +0100

    The code has been made more beautiful by adding a method

--- src/IO/FileHandler.java
+++ src/IO/FileHandler.java
@@ -1,5 +1,8 @@
 package IO;
 
+import exception.InvalidArgumentException;
+import model.Mode;
+
 import java.io.*;
 
 public class FileHandler {
@@ -18,11 +21,42 @@ public class FileHandler {
         return content.toString();
     }
 
-    public void writeStringToFile(String filePath, String content) {
+    public void writeTextToFile(String filePath, String content) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(filePath)))) {
             writer.write(content);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+//    Specify "ENCRYPT" or "DECRYPT" or "BRUTE_FORCE" in the mod line to add to the file name [ENCRYPTED] or [DECRYPTED] or [BRUTE_FORCED]
+    public void writeStringToFile(String filePath, String txt, String mode) {
+        boolean isCommandExist = false;
+        for (Mode mods : Mode.values()) {
+            if (mods.toString().equals(mode.toUpperCase())) {
+                isCommandExist = true;
+                break;
+            }
+        }
+        if (isCommandExist) {
+            int lastIndex = filePath.lastIndexOf(".");
+            StringBuilder stringBuilder = new StringBuilder(filePath);
+            String path1 = stringBuilder.substring(0, lastIndex);
+            String path2 = stringBuilder.substring(lastIndex);
+            switch (mode.toUpperCase()) {
+                case "ENCRYPT":
+                    writeTextToFile(path1 + " [ENCRYPTED]" + path2, txt);
+                    break;
+                case "DECRYPT":
+                    writeTextToFile(path1 + " [DECRYPTED]" + path2, txt);
+                    break;
+                case "BRUTE_FORCE":
+                    writeTextToFile(path1 + " [BRUTE_FORCED]" + path2, txt);
+                    break;
+            }
+        }
+        else {
+            throw new InvalidArgumentException("Select the correct operating mode");
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -13,37 +13,35 @@ public class Main {
     private static final int KEY_ARGUMENT_POSITION = 2;
 
     public static void main(String[] args) {
+
         InputValidation inputValidation = new InputValidation();
         inputValidation.validateInput(args);
         inputValidation.validateCommand(args[COMMAND_ARGUMENT_POSITION]);
         inputValidation.validateFilePath(args[FILE_PATH_ARGUMENT_POSITION]);
+
         if (args.length == 3) {
             inputValidation.validateKey(args[KEY_ARGUMENT_POSITION]);
             FileHandler fileHandler = new FileHandler();
             String text = fileHandler.readFileToString(args[1]);
-
             String filePath = args[1];
-            int lastIndex = filePath.lastIndexOf(".");
-            StringBuilder stringBuilder = new StringBuilder(filePath);
-            String path1 = stringBuilder.substring(0, lastIndex);
-            String path2 = stringBuilder.substring(lastIndex);
+
 
             switch (args[COMMAND_ARGUMENT_POSITION]) {
                 case "ENCRYPT":
                     EncryptService encryptService = new EncryptService();
-                    fileHandler.writeStringToFile(path1 + " [ENCRYPTED]" + path2, encryptService.encrypt(text, Integer.parseInt(args[2])));
+                    fileHandler.writeStringToFile(filePath, encryptService.encrypt(text, Integer.parseInt(args[2])), "ENCRYPT");
                     System.out.println("Successfully encrypted");
                     break;
 
-                case "DECRPYT":
+                case "DECRYPT":
                     DecryptService decryptService = new DecryptService();
-                    fileHandler.writeStringToFile(path1 + " [DECRYPTED]" + path2, decryptService.decrypt(text, -Integer.parseInt(args[2])));
+                    fileHandler.writeStringToFile(filePath, decryptService.decrypt(text, -Integer.parseInt(args[2])), "DECRYPT");
                     System.out.println("Successfully decrypted");
                     break;
 
                 case "BRUTE_FORCE":
                     BruteForceService bruteForceService = new BruteForceService();
-                    fileHandler.writeStringToFile(path1 + " [BRUTE_FORCED]" + path2, bruteForceService.bruteForceDecrypt(text));
+                    fileHandler.writeStringToFile(filePath, bruteForceService.bruteForceDecrypt(text), "BRUTE_FORCE");
                     System.out.println("Successfully brute forced");
             }
         } else {
@@ -51,12 +49,8 @@ public class Main {
             String text = fileHandler.readFileToString(args[1]);
             if (args[COMMAND_ARGUMENT_POSITION].equals("BRUTE_FORCE")) {
                 String filePath = args[1];
-                int lastIndex = filePath.lastIndexOf(".");
-                StringBuilder stringBuilder = new StringBuilder(filePath);
-                String path1 = stringBuilder.substring(0, lastIndex);
-                String path2 = stringBuilder.substring(lastIndex);
                 BruteForceService bruteForceService = new BruteForceService();
-                fileHandler.writeStringToFile(path1 + " [BRUTE_FORCED]" + path2, bruteForceService.bruteForceDecrypt(text));
+                fileHandler.writeStringToFile(filePath, bruteForceService.bruteForceDecrypt(text), "BRUTE_FORCE");
                 System.out.println("Successfully brute forced");
             } else {
                 throw new InvalidArgumentException("For arguments ENCRYPT, DECRYPT you need to set a key (shift)");
