71cd8f0388b94770aea399ea1d9fc6822a4f6e8e
IvanKa <ivankalnykit@gmail.com>
2024-01-27 09:02:06 +0100

    The text has been changed to better understand how the method works.

--- src/service/DecryptService.java
+++ src/service/DecryptService.java
@@ -2,10 +2,10 @@ package service;
 
 public class DecryptService {
 
-    public String decrypt(String text, int key) {
+    public String decrypt(String ciphertext, int key) {
         StringBuilder result = new StringBuilder();
 
-        for (char character : text.toCharArray()) {
+        for (char character : ciphertext.toCharArray()) {
             if (Character.isLetter(character)) {
                 char base = Character.isUpperCase(character) ? 'A' : 'a';
                 char shiftedChar = (char) ((character - base + key) % 26 + base);
