23025265b399ebac78a3ee528908804f60bd6948
IvanKa <ivankalnykit@gmail.com>
2024-01-27 14:13:42 +0100

    Improved structure performance.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 import IO.FileHandler;
 import exception.InvalidArgumentException;
-import model.Mode;
 import service.BruteForceService;
 import service.DecryptService;
 import service.EncryptService;
 import util.InputValidation;
-
 
 
 public class Main {
@@ -23,30 +21,44 @@ public class Main {
             inputValidation.validateKey(args[KEY_ARGUMENT_POSITION]);
             FileHandler fileHandler = new FileHandler();
             String text = fileHandler.readFileToString(args[1]);
+
+            String filePath = args[1];
+            int lastIndex = filePath.lastIndexOf(".");
+            StringBuilder stringBuilder = new StringBuilder(filePath);
+            String path1 = stringBuilder.substring(0, lastIndex);
+            String path2 = stringBuilder.substring(lastIndex);
+
             switch (args[COMMAND_ARGUMENT_POSITION]) {
                 case "ENCRYPT":
                     EncryptService encryptService = new EncryptService();
-                    System.out.println(encryptService.encrypt(text, Integer.parseInt(args[2])));
+                    fileHandler.writeStringToFile(path1 + " [ENCRYPTED]" + path2, encryptService.encrypt(text, Integer.parseInt(args[2])));
+                    System.out.println("Successfully encrypted");
                     break;
 
                 case "DECRPYT":
                     DecryptService decryptService = new DecryptService();
-                    System.out.println(decryptService.decrypt(text, Integer.parseInt(args[2])));
+                    fileHandler.writeStringToFile(path1 + " [DECRYPTED]" + path2, decryptService.decrypt(text, -Integer.parseInt(args[2])));
+                    System.out.println("Successfully decrypted");
                     break;
 
                 case "BRUTE_FORCE":
                     BruteForceService bruteForceService = new BruteForceService();
-                    System.out.println(bruteForceService.bruteForceDecrypt(text));
+                    fileHandler.writeStringToFile(path1 + " [BRUTE_FORCED]" + path2, bruteForceService.bruteForceDecrypt(text));
+                    System.out.println("Successfully brute forced");
             }
-        }
-        else {
+        } else {
             FileHandler fileHandler = new FileHandler();
             String text = fileHandler.readFileToString(args[1]);
             if (args[COMMAND_ARGUMENT_POSITION].equals("BRUTE_FORCE")) {
+                String filePath = args[1];
+                int lastIndex = filePath.lastIndexOf(".");
+                StringBuilder stringBuilder = new StringBuilder(filePath);
+                String path1 = stringBuilder.substring(0, lastIndex);
+                String path2 = stringBuilder.substring(lastIndex);
                 BruteForceService bruteForceService = new BruteForceService();
-                System.out.println(bruteForceService.bruteForceDecrypt(text));
-            }
-            else {
+                fileHandler.writeStringToFile(path1 + " [BRUTE_FORCED]" + path2, bruteForceService.bruteForceDecrypt(text));
+                System.out.println("Successfully brute forced");
+            } else {
                 throw new InvalidArgumentException("For arguments ENCRYPT, DECRYPT you need to set a key (shift)");
             }
         }
