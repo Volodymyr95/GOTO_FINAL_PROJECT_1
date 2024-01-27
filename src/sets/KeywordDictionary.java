6cfac427ccce434df0feed5f4a5fb8fb0264e5db
IvanKa <ivankalnykit@gmail.com>
2024-01-27 08:57:08 +0100

    Added an array of frequently used words in English text

---
+++ src/sets/KeywordDictionary.java
@@ -0,0 +1,8 @@
+package sets;
+
+public class KeywordDictionary {
+    public final String [] KEYWORDS = {"THE", "AND", "IS", "IT", "THAT", "I", "YOU", "NOT", "BUT", "WITH", "WAS", "FOR", "HAVE", "OF",
+            "IN", "TO", "A", "AND", "WITH", "ON", "BY", "FOR", "ABOUT", "THAT", "AS", "FROM", "AT", "THEY", "WE", "SHE", "HE", "HIS",
+            "HER", "THEM", "THEIR", "HIM", "HERS", "HIMSELF", "ITSELF", "YOUR", "OUR", "ITS", "WHICH", "WHOSE", "WHOM", "WAS", "WERE",
+            "IS", "ARE", "CAN", "WILL", "MAY", "MIGHT"};
+}
