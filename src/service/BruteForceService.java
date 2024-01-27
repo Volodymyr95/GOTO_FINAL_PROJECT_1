15426b9af1e26cc88cd2ab9ec9c7669da189e492
IvanKa <ivankalnykit@gmail.com>
2024-01-27 09:01:15 +0100

    Added bruteForceDecrypt method which itself sorts through the keys and finds the correct decryption.

--- src/service/BruteForceService.java
+++ src/service/BruteForceService.java
@@ -1,4 +1,29 @@
 package service;
 
+import sets.KeywordDictionary;
+
 public class BruteForceService {
+
+    public String bruteForceDecrypt(String ciphertext) {
+        DecryptService decryptService = new DecryptService();
+        for (int key = 0; key < 26; key++) {
+            String decryptedText = decryptService.decrypt(ciphertext, key);
+
+            if (containsKeywords(decryptedText)) {
+                return decryptedText;
+            }
+        }
+
+        return null;
+    }
+
+    private boolean containsKeywords(String text) {
+        KeywordDictionary keywordDictionary = new KeywordDictionary();
+        for (String keyword : keywordDictionary.KEYWORDS) {
+            if (!text.toUpperCase().contains(keyword)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
